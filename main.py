5baf31d9e2af6725d52a50a8af64352b807133b0
Dustin Bloomquist <dabpython@gmail.com>
2025-03-18 21:55:34 -0400

    Test new method for checking url of database

--- main.py
+++ main.py
@@ -35,7 +35,12 @@ Bootstrap5(app)
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+db_url = os.getenv("DATABASE_URL")
+if db_url and db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
